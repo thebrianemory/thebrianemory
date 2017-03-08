158c47cd46517a298afea3cbb69c518d54044ec0
thebrianemory <the@brianemory.com>
2017-03-08 11:24:50 -0800

    Create method for grabbing blog feed

--- app/controllers/blog_feed_controller.rb
+++ app/controllers/blog_feed_controller.rb
@@ -1,2 +1,18 @@
 class BlogFeedController < ApplicationController
+
+  def blog_posts
+    require 'rss'
+    rss_results = []
+    rss = RSS::Parser.parse(open('https://medium.brianemory.com/feed').read, false).items[0..2]
+
+    rss.each do |result|
+      result = { title: result.title,
+                 date: result.pubDate.strftime('%A, %d %b %Y at%l:%M %p'),
+                 link: result.link[/[^?]+/],
+                 content: result.content_encoded.sub(/(<figure>).*(<\/figure>)/, '').truncate(200, separator: ' '),
+                 image: result.content_encoded[/(https).*(.jpeg)/] }
+      rss_results.push(result)
+    end
+    rss_results
+  end
 end
