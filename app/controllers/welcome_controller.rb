c26ed795a4f321c0f2836fe9897374b647fbb27e
thebrianemory <the@brianemory.com>
2017-03-08 21:59:20 -0800

    Finalize blog layout and manuall grab of Medium data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  include ActionView::Helpers::SanitizeHelper
 
   def index
     get_blog_posts
@@ -6,7 +7,10 @@ class WelcomeController < ApplicationController
 
   private
   def get_blog_posts
+    # this would be so much easier if the Medium API allowed GET requests for posts
+    # I'll have to clean this up later after it is working
     require 'rss'
+    sanitizer = Rails::Html::FullSanitizer.new
     @posts = []
     rss = RSS::Parser.parse(open('https://medium.brianemory.com/feed').read, false).items[0..2]
 
@@ -14,10 +18,12 @@ class WelcomeController < ApplicationController
       result = { title: result.title,
                  date: result.pubDate.strftime('%A, %d %b %Y at%l:%M %p'),
                  link: result.link[/[^?]+/],
-                 content: result.content_encoded.sub(/(<figure>).*(<\/figure>)/, '').truncate(200, separator: ' '),
-                 image: result.content_encoded[/(https).*(.jpeg)/] }
+                 heading: sanitizer.sanitize(result.content_encoded.sub(/(<h4>|<h3>)/, '<Z>').sub(/(<\/h4>|<\/h3>)/, '<Z>')[/(<Z>).*(<Z>)/]),
+                 content: sanitizer.sanitize(result.content_encoded.sub(/(<figure>).*(<\/figure>)/, '').truncate(300, separator: ' ').sub(/(<h4>|<h3>).*(<\/h4>|<\/h3>)/, '')),
+                 image: result.content_encoded[/(https).*(.jpeg|.png|.jpg)/] }
       @posts.push(result)
     end
     @posts
   end
 end
+
